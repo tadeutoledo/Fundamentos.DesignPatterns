8f1420cba3ec995d22b4674906be3db759aac8e8
Tadeu Mathias <tmtadeu@gmail.com>
2022-06-22 15:51:27 -0300

    Commit inicial do projeot

---
+++ Creational/AbstractFactory/ExecutarAbstractFactory.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fundamentos.DesignPatterns.Creational.AbstractFactory
+{
+  public class ExecutarAbstractFactory
+  {
+    public static void Executar()
+    {
+
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Fundamentos.DesignPatterns.Creational.AbstractFactory;
+
+int opcao = 999;
+
+while (opcao > 0)
+{
+  Console.WriteLine("Digite a opção:");
+  Console.WriteLine("1 - Abstract Factory");
+
+  if (!int.TryParse(Console.Read().ToString(), out opcao))
+    opcao = -1;
+
+  switch (opcao)
+  {
+    case 1:
+      ExecutarAbstractFactory.Executar();
+      break;
+  }
+}
+
